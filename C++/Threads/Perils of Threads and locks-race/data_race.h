500debee29f9d3dff2c9100cc2c3e6b706ac2b03
DmitryMaksyutenko <47276388+DmitryMaksyutenko@users.noreply.github.com>
2019-08-19 12:07:48 +0300

    Update data_race.h
    fixes

--- C++/Threads/Perils of Threads and locks-race/data_race.h
+++ C++/Threads/Perils of Threads and locks-race/data_race.h
@@ -8,6 +8,12 @@ the same object. Atomic objects are free of data races.
 Each instantiation and full specialization of the std::atomic template defines an atomic type. 
 If one thread writes to an atomic object while another thread reads from it, the behavior is well-defined
 https://en.cppreference.com/w/cpp/language/memory_model
+
+A memory barrier:
+    Also known as a membar, memory fence or fence instruction, is a type of barrier instruction 
+that causes a central processing unit or compiler to enforce an ordering constraint on memory operations issued 
+before and after the barrier instruction. This typically means that operations issued prior to the barrier are 
+guaranteed to be performed before operations issued after the barrier.
 */
 
 /*  There will be two threads, they will races for context of storage.
