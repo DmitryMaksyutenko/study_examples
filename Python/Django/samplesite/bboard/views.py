de9590ed9ce37cbf0ad5cd2975739ed86bbdf5ef
Dmitry <1988maksyutenko@gmail.com>
2019-09-10 16:45:17 +0300

    Added templates directory with first simple template, researched the Django shell, added data into databse via shell:

--- Python/Django/samplesite/bboard/views.py
+++ Python/Django/samplesite/bboard/views.py
@@ -1,5 +1,20 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+# from django.template import loader
+from django.shortcuts import render
+
+
+from .models import Db
+
+
+#   def index(request):
+#       """Start page function"""
+#       template = loader.get_template('bboard/index.html')
+#       bbs = Db.objects.order_by('-published')
+#       context = {'bbs': bbs}
+#       return HttpResponse(template.render(context, request))
+
 
 def index(request):
     """Start page function"""
-    return HttpResponse("Здесь будет выведен список объявлений.")
+    bbs = Db.objects.order_by('-published')
+    return render(request, 'bboard/index.html', {'bbs': bbs})
