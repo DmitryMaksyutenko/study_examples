4868e451f5056a61256075df3ec7771cfd7e04ef
Dmitry <1988maksyutenko@gmail.com>
2019-10-13 14:44:08 +0300

    Cities table from database prinrs in HTML table with Django framework.

--- Python/Django/MySQLsettings/settingsMySQL/showWorld/views.py
+++ Python/Django/MySQLsettings/settingsMySQL/showWorld/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
+from . models import City
+
 # Create your views here.
 def index_page(request):
     '''This function returns main page.'''
-    return render(request, 'index.html')
+    title = "Cities"
+    cities = City.objects.all()
+    context = {'title': title, 'cities': cities}
+    return render(request, 'index.html', context)
