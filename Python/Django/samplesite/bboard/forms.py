4d4afdab4aaf0565310b1d0946c7d3b27832c7da
Dmitry <1988maksyutenko@gmail.com>
2019-11-12 14:09:10 +0200

    Cleaning.

--- Python/Django/samplesite/bboard/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import Db
-
-
-class DbForm(ModelForm):
-    class Meta:
-        model = Db
-        fields = ('title', 'content', 'price', 'rubric')
