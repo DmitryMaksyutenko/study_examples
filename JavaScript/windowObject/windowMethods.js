acacb29c4ef5dde4fc32e9c190e69d23f1f9425c
Dmitry <1988maksyutenko@gmail.com>
2019-11-28 19:11:40 +0200

    browser window object scripts.

---
+++ JavaScript/windowObject/windowMethods.js
@@ -0,0 +1,43 @@
+document.write('<p style="text-align: center; font-size: 26px; color: red;">window methods</p>')
+document.write('<hr>')
+
+document.write('<p style="text-align: center; font-size: 26px; color: red;">confirm promt alert</p>')
+
+while(window.confirm("Ready to enter command?")){
+  var res = window.prompt("Enter the command:")
+
+  if(res != null){
+    document.write("<p style='text-align: center;'>"+ res +" = "+ eval(res) +"</p>")
+  }
+  else{
+    break
+  }
+
+}
+
+window.alert("end of scenario!")
+
+document.write('<hr>')
+
+document.write('<p style="text-align: center; font-size: 26px; color: red;">open close</p>')
+
+function openWindow(){
+  if(window.mywindow && !mywindow.closed){
+    window.alert("the document already opend!")
+  }
+  else{
+    window.mywindow = window.open("hello.html")
+  }
+}
+
+function closeWindow(){
+  if(window.mywindow && !mywindow.closed){
+    mywindow.close()
+    window.mywindow = null
+  }
+  else{
+    window.alert("the window already closed")
+  }
+}
+
+document.write('<hr>')
