8367c999d9453ed1153efa96c194ea39059e798c
Dmitry <1988maksyutenko@gmail.com>
2020-03-25 20:20:22 +0200

    jQuery performance tips.

---
+++ JavaScript/jQuery/Performance/UseStylesheets/example.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    let $div = $("#d");
+
+    $("<style type=\"text/css\"> a {color: red; text-decoration: none;}</style>").
+                                    appendTo($div);
+
+});
