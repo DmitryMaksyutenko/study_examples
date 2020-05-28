86819cb23b2bb8dcea580309339fd98f330e2388
DmitryMaksyutenko <1988maksyutenko@gmail.com>
2020-05-28 15:36:35 +0300

    PostgreSQL FULL OUTER JOIN with foreign key example.

--- SQL/PostgreSQL/Join/full_outer_join.sql
+++ SQL/PostgreSQL/Join/full_outer_join.sql
@@ -19,3 +19,15 @@ FROM
 FULL JOIN basket_b b ON
     a.fruit = b.fruit
     WHERE a.id IS NULL OR b.id IS NULL;
+
+
+-- JOIN with foreign key.
+SELECT
+    actor.actor_id,
+    actor.first_name,
+    actor.last_name,
+    LEFT(film_info, 150) film_info
+FROM
+    actor
+FULL OUTER JOIN actor_info ON
+    actor.actor_id = actor_info.actor_id;
