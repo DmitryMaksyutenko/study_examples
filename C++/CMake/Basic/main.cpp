cd32bd140461f4e7be8bea3897e8d8868ba269c5
Dima-Home <1988maksyutenko@gmail.com>
2020-10-08 17:47:50 +0300

    CMake base example finished.

--- C++/CMake/Basic/main.cpp
+++ C++/CMake/Basic/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 
+#include <config.h>
+
 
 int main(int argc, char **argv)
 {
+    if (argc < 2) {
+        // report version
+        std::cout << argv[0] << " Version "
+                  << baseapp_VERSION_MAJOR << "."
+                  << baseapp_VERSION_MINOR << std::endl;
+        std::cout << "Usage: " << argv[0] << " number" << std::endl;
+        return 1;
+    }
+
     std::cout << "Hello World!\n";
 
     return 0;
