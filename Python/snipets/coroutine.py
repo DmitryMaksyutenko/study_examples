4da2c95284665d07aad0853272022fded352abc7
Dima-homeL <1988maksyutenko@gmail.com>
2020-12-21 11:29:05 +0200

    Simple asyncio with rundom_sleep.

--- Python/snipets/coroutine.py
+++ Python/snipets/coroutine.py
@@ -11,10 +11,14 @@
 # at set points by programmer.
 
 # In Python, coroutines are similar to generators but with few extra methods
-# and slight change in how we use yield statement.
+# and slight change in how used yield statement.
 # Generators produce data for iteration while coroutines can also consume data.
+# Both return the control of the program.
+import asyncio
+from random import randint
 
 
+# Simple example.
 def print_name(name):
     print(f"This is my name - {name}")
     while True:
@@ -28,3 +32,25 @@ next(coroutine)
 coroutine.send("George")
 coroutine.send("Sam")
 coroutine.close()
+
+
+# Base asyncio example.
+async def random_sleep():
+    await asyncio.gather(counter_1(), counter_2())
+
+
+async def counter_1():
+    print("start counter 1")
+    rand_num = randint(1, 20)
+    await asyncio.sleep(rand_num)
+    print(f"end counter 1 after {rand_num} sec.")
+
+
+async def counter_2():
+    print("start counter 2")
+    rand_num = randint(1, 20)
+    await asyncio.sleep(rand_num)
+    print(f"end counter 2 after {rand_num} sec.")
+
+
+asyncio.run(random_sleep())
