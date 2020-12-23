4104d508a00abbcd86499ebdfc033dc2f34ad0b6
Dima-homeL <1988maksyutenko@gmail.com>
2020-12-23 10:03:54 +0200

    Generator send StopIteration in throw method.

--- Python/snipets/iterator_generator.py
+++ Python/snipets/iterator_generator.py
@@ -1,7 +1,8 @@
-# An "iterable" is any object in Python which has an __iter__ or a __getitem__ 
-# method defined which returns an iterator or can take indexes
+# An "iterable" is any object in Python which has an __iter__ or a __getitem__
+# method implemented which returns an iterator or can take indexes
 
 # An "iterator" is any object in Python which has a __next__ method defined.
+
 
 class MyIterator:
     def __init__(self, start=0, stop=None, step=1):
@@ -27,17 +28,22 @@ print("Iterator: ")
 for i in itr:
     print(i, end=", ")
 
-# Generator functions are a special kind of function that return a lazy iterator. 
-# These are objects that can loop over objects like a list. However, unlike lists, 
-# lazy iterators do not store their contents in memory. 
+# Generator functions are a special kind of function that return a lazy
+# iterator. These are objects that can loop over objects like a list.
+# However, unlike lists, lazy iterators do not store their contents in memory.
+# The generator can remember the state of it's execution, suspend it, return
+# control to the caller, and resume execution from remembered state.
+
 
 def simple_generator():
     for number in MyIterator(0, 20):
         yield number
 
+
 print("\nGenerator: ")
 for item in simple_generator():
     print(item, end=", ")
+
 
 def fibonacci(n):
     num = next_num = 1
@@ -45,6 +51,21 @@ def fibonacci(n):
         num, next_num = next_num, num + i
         yield num
 
+
 print("\nFibonacci: ")
 for num in fibonacci(100):
     print(num, end=", ")
+
+
+def show_words():
+    try:
+        while True:
+            try:
+                print("BEFORE YIELD")
+                word = yield
+                print(word)
+            except StopIteration:
+                print("EXCEPT INNER")
+    except StopIteration:
+        print("EXCEPT UOTER")
+        return
