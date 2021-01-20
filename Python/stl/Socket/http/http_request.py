cd3ca1cacc994246de8640cb8c28d463fec18e27
Dima-homeL <1988maksyutenko@gmail.com>
2021-01-20 16:32:48 +0200

    part of HTTP response received.

--- Python/stl/Socket/http/http_request.py
+++ Python/stl/Socket/http/http_request.py
@@ -1,23 +1,37 @@
 import socket
+import pprint
 import sys
 
 
-def get_address_from_cmd():
+def parse_cmd():
     if len(sys.argv) == 3:
         return (sys.argv[1], sys.argv[2])
     else:
         print("python http_request.py 'host' 'method'")
 
 
-def connect(host, port=80):
-    return socket.create_connection((host, port), 2)
+def split_address(addr):
+    if "/" in addr:
+        index = addr.find("/")
+        return (addr[:index], addr[index:])
+    else:
+        return (addr, "")
+
+
+def create_http_query(*args):
+    method, path = args
+    if path == "":
+        path = "/"
+    return bytes(method + " " + path + " HTTP/1.1\n\n", "ascii")
 
 
 if __name__ == "__main__":
-    addr, method = get_address_from_cmd()
-    connection = connect(addr)
-
-    http_query = bytes(method + " / HTTP/1.1\n\n", "ascii")
-
+    addr, method = parse_cmd()
+    domain, path = split_address(addr)
+    connection = socket.create_connection((domain, 80), 2)
+    http_query = create_http_query(method, path)
+    print(http_query)
     connection.send(http_query)
-    print(connection.recv(4098))
+    response = connection.recv(1024, socket.MSG_WAITALL)
+    connection.close()
+    pprint.pprint(response)
--- Python/stl/Socket/http/tst_http_request.py
+++ Python/stl/Socket/http/tst_http_request.py
@@ -1,8 +1,7 @@
-from socket import socket
-
 from http_request import (
-    get_address_from_cmd,
-    connect
+    parse_cmd,
+    split_address,
+    create_http_query
 )
 
 
@@ -10,15 +9,29 @@ def test_get_addres_from_cmd(monkeypatch):
     host = "tst.com"
     method = "GET"
     monkeypatch.setattr("sys.argv", ["file", host, method])
-    data_host, data_method = get_address_from_cmd()
+    data_host, data_method = parse_cmd()
 
-    assert isinstance(get_address_from_cmd(), tuple)
+    assert isinstance(parse_cmd(), tuple)
     assert data_host == host
     assert data_method == method
 
 
-def test_connection():
-    host = "google.com"
-    con = connect(host)
+def test_split_addres():
+    domain = "some.com"
+    resource_path = "/test/path/page.html"
+    res = split_address(domain + resource_path)
+    dom, path = res
 
-    assert isinstance(con, socket)
+    assert isinstance(res, tuple)
+    assert dom == domain
+    assert path == resource_path
+
+
+def test_create_http_query():
+    method = "GET"
+    path = "/tst/path/index.html"
+    test_query = bytes(method + " " + path + " HTTP/1.1\n\n", "ascii")
+    http_query = create_http_query(method, path)
+
+    assert isinstance(http_query, bytes)
+    assert http_query == test_query
