4cf52c8510b67182048ff3ac6ab4c55424083901
Dima-homeL <1988maksyutenko@gmail.com>
2021-02-18 08:08:20 +0200

    Infinitie functions form itertools.

---
+++ Python/stl/itertools/infinite_iterators.py
@@ -0,0 +1,25 @@
+import itertools
+# count()
+# cycle()
+# repeat()
+
+
+def from_num_to_infinite(num: int) -> iter:
+    """Generate numbers from passed value to infinitie."""
+    return itertools.count(num, 1)
+
+
+def collection_by_cycle(collection) -> iter:
+    """Iterates through the collection in round."""
+    return itertools.cycle(collection)
+
+
+def repeat_message_n_times(message: str, n: int) -> iter:
+    """Prints the message n times."""
+    for mess in itertools.repeat(message, n):
+        print(mess)
+
+
+# Printing global table.
+import pprint  # noqa
+pprint.pprint(globals())
